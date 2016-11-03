65df94e66516739694fff34693a2ed85a30e24f7
Brian Barrow <briancbarrow@gmail.com>
2016-11-03 13:22:35 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+var jsonfile = require('jsonfile');
+var app = express();
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080);
+
+// var getFromApi = function(args) {
+//     var emitter = new events.EventEmitter();
+//     unirest.post('https://probasketballapi.com/stats/players/')
+//           .qs(args)
+//           .end(function(response) {
+//                 if (response.ok) {
+//                     emitter.emit('end', response.body);
+//                     jsonfile.writeFile('public/data.json', response.body, function (err) {
+//                       console.error(err);
+//                     });
+//                 }
+//                 else {
+//                     emitter.emit('error', response.code);
+//                 }
+//             });
+//     jsonfile.writeFile('public/data.json', emitter, function (err) {
+//       console.error(err);
+//     });
+//     // return emitter;
+// };
+
+// getFromApi({api_key: 'K6Rk27YUc5philzJXBjAevmQn0aNSsfD', season: '2015', team_id: 1610612745});
